53ae240d461d711e9f69a7f93a7d01404b848093
adRn-s <lims@omics.dev>
2023-09-19 16:31:07 +0200

    rdy

--- instant_backup.sh
+++ instant_backup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-directory=$(dirname -- $(readlink -fn -- "$0"))
 NOW=$(date +%Y%m%d-%H%M%S)
-find $directory -name '*.sqldump' -ctime +7 -delete
-docker exec vikunja-db pg_dump -Fc postgres -U vikunja -f sqldump
-docker cp vikunja-db:/sqldump $directory/$NOW.sqldump
-ln -sf $directory/$NOW.sqldump $directory/latest.sqldump
+directory=$(dirname -- $(readlink -fn -- "$0"))
+mkdir -p ${directory}/dumps/
+docker exec -it vikunja-api sh -c "rm -f /app/vikunja/*.zip"
+docker exec -it vikunja-api /app/vikunja/vikunja dump
+docker exec -it vikunja-api sh -c "ln -sf /app/vikunja/vikunja-dump_*.zip /app/vikunja/dump.zip"
+docker cp -L vikunja-api:/app/vikunja/dump.zip ${directory}/dumps/${NOW}.zip
